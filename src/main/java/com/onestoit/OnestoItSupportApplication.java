76c5ae85592a824ad6c0bf20c76d6cb1e2490f66
22jy0126 <22jy0126@PC38.jynet.jec.ac.jp>
2023-11-07 12:24:20 +0900

    オネストシステムの初期化

---
+++ src/main/java/com/onestoit/OnestoItSupportApplication.java
@@ -0,0 +1,13 @@
+package com.onestoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnestoItSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnestoItSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onestoit/OnestoItSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onestoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnestoItSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
