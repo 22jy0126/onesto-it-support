9de4dcc80585eca2ce7cb411fb161cb3b76ef749
22jy0126 <22jy0126@PC38.jynet.jec.ac.jp>
2023-12-12 10:09:57 +0900

    技術を定数にする

---
+++ src/main/resources/static/js/consts.js
@@ -0,0 +1,37 @@
+/**
+ * 技術項目 
+*/
+const techs = [{
+    value: "VBA",
+    label: "VBA",
+}, {
+    value: "java",
+    label: "java",
+}, {
+    value: "c++",
+    label: "c++",
+}, {
+    value: "c#",
+    label: "c#",
+}, {
+    value: "php",
+    label: "php",
+}, {
+    value: "python",
+    label: "python",
+}, {
+    value: "html",
+    label: "html",
+}, {
+    value: "css",
+    label: "css",
+}, {
+    value: "javaScript",
+    label: "javaScript",
+}, {
+    value: "android",
+    label: "android",
+}, {
+    value: "ios",
+    label: "ios",
+}];
