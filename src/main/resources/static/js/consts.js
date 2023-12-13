493cd15235fd00f05127d7671f0b9b00eb1a7c61
22jy0126 <22jy0126@PC38.jynet.jec.ac.jp>
2023-12-13 14:38:31 +0900

    マイルストーンの状態

--- src/main/resources/static/js/consts.js
+++ src/main/resources/static/js/consts.js
@@ -35,3 +35,24 @@ const techs = [{
     value: "ios",
     label: "ios",
 }];
+
+/**
+ * マイルストーン進捗状態
+*/
+const milestoneStatus = [{
+    code: "1",
+    text: "作業未開始",
+    color: "#d1d1d1",
+}, {
+    code: "2",
+    text: "作業中",
+    color: "#409eff",
+}, {
+    code: "3",
+    text: "作業完了",
+    color: "#a7e987",
+}, {
+    code: "4",
+    text: "確認済み",
+    color: "#67c23a",
+}];
